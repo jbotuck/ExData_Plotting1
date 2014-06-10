0947f0adef1cb1f2c04ad52e3e7289e767e1b8b5
Jacob Botuck <jbotuck@gmail.com>
2014-06-10 10:16:40 -0500

    fixed data reading so that plot3 will now display properly, updated pngs 3 & 4

--- helpers.R
+++ helpers.R
@@ -1,5 +1,5 @@
 read_file <- function(){
-  x <-read.csv2("household_power_consumption.txt", na.strings = "?")
+  x <-read.csv2("household_power_consumption.txt", na.strings = "?", as.is = TRUE)
   x[x$Date %in% c('1/2/2007','2/2/2007'),]
 }
 date_time <- function(date, time){
